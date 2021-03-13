e289bc134ad6d019cfa98a416daf8612e2ea3390
Ameno1807 <jelezovsergei@yandex.ru>
2021-03-13 11:25:32 +0300

    fragment_menu

---
+++ app/src/main/java/hackathon/tastemydroid/menu/FragmentMenu.kt
@@ -0,0 +1,37 @@
+package hackathon.tastemydroid.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import hackathon.tastemydroid.R
+
+
+class FragmentMenu : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<RecyclerView>(R.id.recyclerView_monday).apply {
+            this.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, true)
+        }
+    }
+
+
+
+
+}

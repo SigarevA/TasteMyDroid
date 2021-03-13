d985ea8c6d009b52ea4e11c8b40b7823badd7098
e.zaharova <zakh14@bk.ru>
2021-03-13 17:52:50 +0300

    add functions for bd

--- app/src/main/java/hackathon/tastemydroid/db/MenuDao.kt
+++ app/src/main/java/hackathon/tastemydroid/db/MenuDao.kt
@@ -3,10 +3,21 @@ package hackathon.tastemydroid.db
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import hackathon.tastemydroid.entities.Ingredient
 import hackathon.tastemydroid.entities.Recipe
 
 @Dao
 interface MenuDao {
-    @Query("SELECT * FROM Recipes ")
+    @Query("SELECT * FROM Recipes")
     suspend fun getAllRecipes(): List<Recipe>
+
+    @Insert
+    suspend fun addRecipe(recipe: Recipe)
+
+    @Insert
+    suspend fun addMenu(menu: MenuEntity)
+
+    @Insert
+    suspend fun addIngredient(ingredient: Ingredient)
+
 }

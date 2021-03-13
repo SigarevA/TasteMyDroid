5ccc463053f121a29dddd913dd231ac1234dd222
e.zaharova <zakh14@bk.ru>
2021-03-13 10:39:38 +0300

    remove recipe list in fragment folder

--- app/src/main/java/hackathon/tastemydroid/ui/fragments/RecipeListFragment.kt
+++ app/src/main/java/hackathon/tastemydroid/ui/fragments/RecipeListFragment.kt
@@ -1,12 +1,13 @@
-package hackathon.tastemydroid
+package hackathon.tastemydroid.ui.fragments
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import hackathon.tastemydroid.R
 
-class RecipeFragment : Fragment(R.layout.fragment_recipe) {
+class RecipeListFragment : Fragment(R.layout.fragment_recipe_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
